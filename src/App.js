c49291e4d54f76c97bf656814289c5f0f2a44a8e
daniloBraj <165780340+daniloBraj@users.noreply.github.com>
2024-05-22 14:20:44 +0100

    fixed the generation of quotes

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import { useEffect } from "react";
 
 import "./App.css";
 
-const App = () => { 
-  const [quote, setQuote] = useState(null); 
+const App = () => {
   const [error, setError] = useState(null);
+  const [quote, setQuote] = useState(null);
 
-  useEffect(() => { 
-    const fetchQuote = async () => { 
-      try { 
-        const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes'); 
-        if (!response.ok) { 
-          throw new Error(response.statusText); 
-        } 
-        const data = await response.json(); 
-        setQuote(data); 
-      } catch (err) { 
-        setError('Could not fetch data'); 
-        console.log(err.message); 
-      } 
-    }; 
-    fetchQuote(); 
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(
+        "https://api.breakingbadquotes.xyz/v1/quotes"
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const result = await response.json();
+      setQuote(result[0]);
+    } catch (err) {
+      setError("Could not fetch data");
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    fetchQuote();
   }, []);
 
   return (
     <div>
       {error && <p>{error}</p>}
       <h1>API Project</h1>
-      {data ? (
+      <div>
+        {quote ? (
           <div>
             <h2>{quote.quote}</h2>
             <p>{quote.author}</p>
@@ -36,7 +39,9 @@ const App = () => {
         ) : (
           <p>No quote available</p>
         )}
+      </div>
+      <button onClick={fetchQuote}>Generate Quote</button>
     </div>
-  )
-}
+  );
+};
 export default App;
